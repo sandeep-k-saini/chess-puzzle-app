10a4982fd05362f69380003b295ae89944aa0022
mukeshideio <mukeshideiosoft@gmail.com>
2025-11-24 16:36:30 +0530

    new changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-const config = {
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
-  },
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
 };
-
-export default config;
